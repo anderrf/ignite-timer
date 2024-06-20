6f308a6caa3e0d9e525fdfb9e19cf6cc86b8c640
anderrf <anderson_rocha_farias@outlook.com>
2024-06-19 23:32:33 -0300

    addition of lint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,24 +1,24 @@
-import styled, {css} from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
-interface ButtonContainerProps{
-    variant: ButtonVariant
+interface ButtonContainerProps {
+  variant: ButtonVariant
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green'
+  primary: 'purple',
+  secondary: 'orange',
+  danger: 'red',
+  success: 'green',
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    border: 0;
-    border-radius: 8px;
-    margin: 8px;
-    color: ${props => props.theme['white']};
-    background-color: ${props => props.theme['green-500']}
+  width: 100px;
+  height: 40px;
+  border: 0;
+  border-radius: 8px;
+  margin: 8px;
+  color: ${(props) => props.theme.white};
+  background-color: ${(props) => props.theme['green-500']};
 `

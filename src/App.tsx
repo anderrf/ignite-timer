27323a7cd4ca7f3e9330fd1e0e57b171cb0dad18
anderrf <anderson_rocha_farias@outlook.com>
2024-06-19 22:51:56 -0300

    button test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
-import styles from './App.module.css'
+import {ThemeProvider} from 'styled-components'
+import { Button } from "./components/Button";
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
 
   return (
-    <div>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant={'primary'}/>
+      <Button variant={'secondary'}/>
+      <Button variant={'danger'}/>
+      <Button variant={'success'}/>
+      <Button/>
+    </ThemeProvider>
   )
 }

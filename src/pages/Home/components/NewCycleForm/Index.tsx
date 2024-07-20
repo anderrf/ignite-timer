1e52edb0d4ab2bc983de058f6f1a03cf14d394ad
anderrf <anderson_rocha_farias@outlook.com>
2024-07-20 15:13:32 -0300

    use of context api

---
+++ src/pages/Home/components/NewCycleForm/Index.tsx
@@ -0,0 +1,40 @@
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+
+import { CyclesContext } from '../../Index'
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+export function NewCycleForm() {
+  const { register } = useFormContext()
+  const { activeCycle } = useContext(CyclesContext)
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        placeholder="Dê um nome para o seu projeto"
+        list="task-suggestions"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+      <datalist id="task-suggestions">
+        <option value="1" />
+        <option value="2" />
+        <option value="3" />
+      </datalist>
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        placeholder="00"
+        id="minutesAmount"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
